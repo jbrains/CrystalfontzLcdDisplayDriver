b097e9b54abed5f1299badc373e16b068281d0a4
J. B. Rainsberger <me@jbrains.ca>
2009-10-09 15:45:41 +0300

    InMemoryCatalog returns null correctly for findPrice().

--- Architecture Without Trying With Interfaces/test/source/ca/jbrains/pos/test/InMemoryCatalogTest.java
+++ Architecture Without Trying With Interfaces/test/source/ca/jbrains/pos/test/InMemoryCatalogTest.java
@@ -1,5 +1,8 @@
 package ca.jbrains.pos.test;
 
+import static org.junit.Assert.*;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,24 +13,35 @@ import ca.jbrains.pos.test.RingUpSaleTest.Catalog;
 
 public class InMemoryCatalogTest {
 	public static class InMemoryCatalog implements Catalog {
+		private final Map<String, Price> pricesByBarcode;
+
+		public InMemoryCatalog(Map<String, Price> pricesByBarcode) {
+			this.pricesByBarcode = pricesByBarcode;
+		}
+
 		public static InMemoryCatalog with(Map<String, Price> pricesByBarcode) {
-			return new InMemoryCatalog();
+			return new InMemoryCatalog(pricesByBarcode);
 		}
 
 		@Override
 		public Price findPrice(String barcode) {
-			return Price.lira(795);
+			return pricesByBarcode.get(barcode);
 		}
 	}
 
 	@Test
 	public void foundBarcode() throws Exception {
-		Catalog catalog = InMemoryCatalog
-				.with(new HashMap<String, Price>() {
-					{
-						put("12345", Price.lira(795));
-					}
-				});
+		Catalog catalog = InMemoryCatalog.with(new HashMap<String, Price>() {
+			{
+				put("12345", Price.lira(795));
+			}
+		});
 		Assert.assertEquals(Price.lira(795), catalog.findPrice("12345"));
+	}
+	
+	@Test
+	public void notFoundBarcode() throws Exception {
+		Catalog catalog = InMemoryCatalog.with(Collections.EMPTY_MAP);
+		Assert.assertNull(catalog.findPrice("12345"));
 	}
 }
