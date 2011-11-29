8cc20955fc05c3e6fbc1f3cae287e4a8e9a0cdf8
J. B. Rainsberger <me@jbrains.ca>
2011-11-29 16:31:14 +0100

    Moved dependency on Crystalfontz to another file so that the Sinatra server could use it.

--- lcd_display/lib/lcd_display_udp_server.rb
+++ lcd_display/lib/lcd_display_udp_server.rb
@@ -1,36 +1,5 @@
-require "serialport"
 require "socket"
-
-module Crystalfontz
-  module USB
-    module LCD
-      class Display
-        def self.open(&block)
-          yield self.new(SerialPort.open("/dev/tty.usbserial-CF006760", 19200))
-        end
-
-        def initialize(serial_port)
-          @serial_port = serial_port
-        end
-
-        def clear_display
-          write_to_display(" " * (4 * 20))
-        end
-
-        def write_to_display(text)
-          [17, 0, 0].each { |each| @serial_port.putc each }
-          @serial_port.write(text)
-        end
-
-        def print_message(message)
-          text = message.to_s
-          clear_display
-          write_to_display(text)
-        end
-      end
-    end
-  end
-end
+require "./crystalfontz"
 
 def open_udp_server(port)
   server = UDPSocket.new
