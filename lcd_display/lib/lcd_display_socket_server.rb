365cd26846c959c7333f888a6a1fd023bba8159d
J. B. Rainsberger <me@jbrains.ca>
2011-11-29 15:18:21 +0100

    Made these two more similar.

--- lcd_display/lib/lcd_display_socket_server.rb
+++ lcd_display/lib/lcd_display_socket_server.rb
@@ -3,7 +3,7 @@ require 'serialport'
 
 SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
   lcd_display_server = TCPServer.open(5357)
-  puts "Started server"
+  puts "Started TCP server"
   loop do
     Thread.start(lcd_display_server.accept) do |client|
       puts "Accepted client #{client.inspect}"
@@ -15,6 +15,7 @@ SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
       [17, 0, 0].each { |each| serial_port.putc each }
       serial_port.write(text)
       puts "Wrote text"
+      puts "-----"
       client.close
     end
   end
--- lcd_display/lib/lcd_display_udp_server.rb
+++ lcd_display/lib/lcd_display_udp_server.rb
@@ -7,7 +7,8 @@ SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
   puts "Started UDP server"
   loop do
     text, sender = server.recvfrom(1024)
-    puts "Sender #{sender} sent text #{text}"
+    puts "Accepted client #{sender.inspect}"
+    puts "Received text #{text}"
     puts "Writing text..."
     [17, 0, 0].each { |each| serial_port.putc(each) }
     serial_port.write(" " * 80)
