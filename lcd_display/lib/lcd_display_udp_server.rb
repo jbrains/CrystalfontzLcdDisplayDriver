52b7724aa6a4ded6d7a3684f3c7b3a269e7ddc1f
J. B. Rainsberger <me@jbrains.ca>
2011-11-29 16:13:11 +0100

    The UDP server appears to work now.

--- lcd_display/lib/lcd_display_udp_server.rb
+++ lcd_display/lib/lcd_display_udp_server.rb
@@ -3,7 +3,7 @@ require "socket"
 
 def open_udp_server(port)
   server = UDPSocket.new
-  server.bind(nil, port)
+  server.bind("localhost", port)
   server
 end
 
@@ -14,18 +14,22 @@ def write_text_to_serial_port(text, serial_port)
   serial_port.write(text)
 end
 
-SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
-  udp_port = 5358
-  lcd_display_server = open_udp_server(udp_port)
-  puts "Started UDP server on port #{udp_port}"
-  loop do
-    text, sender = lcd_display_server.recvfrom(1024)
-    puts "Accepted client #{sender.inspect}"
-    puts "Received text #{text}"
-    puts "Writing text..."
-    write_text_to_serial_port(text, serial_port)
-    puts "Wrote text"
-    puts "-----"
+def run_server(options)
+  SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
+    udp_port = options[:port].to_i
+    lcd_display_server = open_udp_server(udp_port)
+    puts "Started UDP server on port #{udp_port}"
+    loop do
+      text, sender = lcd_display_server.recvfrom(1024)
+      puts "Accepted client #{sender.inspect}"
+      puts "Received text #{text}"
+      puts "Writing text..."
+      write_text_to_serial_port(text, serial_port)
+      puts "Wrote text"
+      puts "-----"
+    end
   end
 end
+
+run_server(:port => ARGV[0].to_i || 5358)
 
