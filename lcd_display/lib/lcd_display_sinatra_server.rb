02ffcb2d9072a023d927f580ef681bd623b78c34
J. B. Rainsberger <me@jbrains.ca>
2011-11-29 16:54:30 +0100

    Server appears to work; need to remove some code.

--- lcd_display/lib/lcd_display_sinatra_server.rb
+++ lcd_display/lib/lcd_display_sinatra_server.rb
@@ -2,7 +2,37 @@ require "rubygems"
 require "sinatra"
 require "./crystalfontz"
 
+set :environment, :development
+enable :logging
+
 class LcdDisplayServer < Sinatra::Base
+  post '/display' do
+    text = params[:text]
+    begin
+      Crystalfontz::USB::LCD::Display.open() do | display |
+        display.print_message(text)
+      end
+      redirect "/"
+    rescue
+      return $!
+    end
+  end
+
+  get '/' do
+    <<-HTML
+    <html>
+      <head><title>LCD Display Client</title></head>
+      <body>
+        <form action="/display" method="post">
+          <label for="text" accesskey="m">Message</label>
+          <input type="textarea" name="text" rows="4" cols="20" />
+          <input type="submit" />
+        </form>
+      </body>
+    </html>
+HTML
+  end
+
   get '/message/:text' do
     puts params[:text]
     text = params[:text]
