6a941836f70deff3bd0870b221f0a6abb8849dd6
J. B. Rainsberger <me@jbrains.ca>
2011-11-29 15:31:28 +0100

    Preparing to try removing the thread from the TCP server version.

--- lcd_display/lib/lcd_display_socket_server.rb
+++ lcd_display/lib/lcd_display_socket_server.rb
@@ -5,7 +5,12 @@ def open_server
   TCPServer.open(5357)
 end
 
-
+def write_text_to_serial_port(text, serial_port)
+  [17, 0, 0].each { |each| serial_port.putc each }
+  serial_port.write " " * 80
+  [17, 0, 0].each { |each| serial_port.putc each }
+  serial_port.write(text)
+end
 
 SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
   lcd_display_server = open_server()
@@ -17,10 +22,7 @@ SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
       text = client.read
       puts "Received text #{text}"
       puts "Writing text..."
-      [17, 0, 0].each { |each| serial_port.putc each }
-      serial_port.write " " * 80
-      [17, 0, 0].each { |each| serial_port.putc each }
-      serial_port.write(text)
+      write_text_to_serial_port(text, serial_port)
       puts "Wrote text"
       puts "-----"
       client.close
--- lcd_display/lib/lcd_display_udp_server.rb
+++ lcd_display/lib/lcd_display_udp_server.rb
@@ -7,6 +7,13 @@ def open_server
   server
 end
 
+def write_text_to_serial_port(text, serial_port)
+  [17, 0, 0].each { |each| serial_port.putc each }
+  serial_port.write " " * 80
+  [17, 0, 0].each { |each| serial_port.putc each }
+  serial_port.write(text)
+end
+
 SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
   lcd_display_server = open_server()
   server_type = "UDP server"
@@ -16,10 +23,7 @@ SerialPort.open("/dev/tty.usbserial-CF006760", 19200) do |serial_port|
     puts "Accepted client #{sender.inspect}"
     puts "Received text #{text}"
     puts "Writing text..."
-    [17, 0, 0].each { |each| serial_port.putc(each) }
-    serial_port.write(" " * 80)
-    [17, 0, 0].each { |each| serial_port.putc(each) }
-    serial_port.write(text)
+    write_text_to_serial_port(text, serial_port)
     puts "Wrote text"
     puts "-----"
   end
